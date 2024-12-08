7f199d46a9470a446ef853d288dc07591afada18
Akshay Patil <53465888+AkshayPatil4@users.noreply.github.com>
2024-12-08 01:39:26 +0100

    Added environmet files to hide firebase auth keys

--- coding-challenges-platform/src/app/app.config.ts
+++ coding-challenges-platform/src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +18,8 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
       },
-    }), provideFirebaseApp(() => initializeApp({"projectId":"coding-challenges-platform","appId":"1:746757611919:web:8bf46c986a36274d2b6bfa","storageBucket":"coding-challenges-platform.firebasestorage.app","apiKey":"AIzaSyBVQpHYC6PYa3a6PX0TXDE7SeBIP4JYlP4","authDomain":"coding-challenges-platform.firebaseapp.com","messagingSenderId":"746757611919","measurementId":"G-842MR7GVZ6"})), provideAuth(() => getAuth()),
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
 };
---
+++ coding-challenges-platform/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebase: {
+        projectId:"coding-challenges-platform",
+        appId:"1:746757611919:web:8bf46c986a36274d2b6bfa",
+        storageBucket:"coding-challenges-platform.firebasestorage.app",
+        apiKey:"AIzaSyBVQpHYC6PYa3a6PX0TXDE7SeBIP4JYlP4",
+        authDomain:"coding-challenges-platform.firebaseapp.com",
+        messagingSenderId:"746757611919",
+        measurementId:"G-842MR7GVZ6"
+    },
+  };
+  
